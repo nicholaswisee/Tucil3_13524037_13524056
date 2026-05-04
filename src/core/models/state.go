ea2cdfc9d16274ce4a2e3537b90b1b93e034ce4b
Alfonzo <reinhardalfonso@gmail.com>
2026-05-04 19:49:59 +0700

    feat: implemented sliding mechanics and add priority queue data structure

--- src/core/models/state.go
+++ src/core/models/state.go
@@ -14,3 +14,59 @@ type MoveRecord struct {
 	NewPos    Position
 	MoveCost  int
 }
+
+type StateKey struct {
+	Pos     Position
+	NextNum int
+}
+
+func (s *GameState) GetKey() StateKey {
+	return StateKey{Pos: s.Pos, NextNum: s.NextNum}
+}
+
+func (s *GameState) Slide(m *MapData, d Direction) (GameState, int, bool) {
+	currPos := s.Pos
+	cost := 0
+	nextNum := s.NextNum
+
+	for {
+		nextPos := currPos.Add(d.DxDy())
+
+		if !m.InBounds(nextPos) {
+			return GameState{}, 0, false
+		}
+
+		tile := m.TileAt(nextPos)
+
+		if tile == TileWall {
+			break
+		}
+
+		if tile == TileLava {
+			return GameState{}, 0, false
+		}
+
+		currPos = nextPos
+		cost += m.CostAt(currPos)
+
+		if tile == TileNumber {
+			ch := m.Grid[currPos.X][currPos.Y]
+			numInjected := int(ch - '0')
+
+			if numInjected == nextNum {
+				nextNum++
+				if nextNum >= m.TotalNumbers {
+					nextNum = -1
+				}
+			} else if numInjected > nextNum {
+				return GameState{}, 0, false
+			}
+		}
+	}
+
+	if currPos == s.Pos {
+		return GameState{}, 0, false
+	}
+
+	return GameState{Pos: currPos, NextNum: nextNum}, cost, true
+}
---
+++ src/core/queue/pq.go
@@ -0,0 +1,42 @@
+package queue
+
+import "github.com/nicholaswisee/Tucil3_13524037_13524056/core/models"
+
+type SearchNode struct {
+	State       models.GameState
+	Cost        int
+	Path        []models.MoveRecord
+	PathHistory []models.Position
+	Index       int
+}
+
+type PriorityQueue []*SearchNode
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Cost < pq[j].Cost
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*SearchNode)
+	item.Index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.Index = -1
+	*pq = old[0 : n-1]
+	return item
+}
